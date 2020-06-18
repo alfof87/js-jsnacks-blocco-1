b759b45f8d8f788cf7f203bf770842deced8f504
Alfonso <alfof87@gmail.com>
2020-06-18 15:21:55 +0200

    jsnack-1

---
+++ jsnack-1/js/script.js
@@ -0,0 +1,26 @@
+var numeroA = prompt("Inserisci un numero");
+var numeroB = prompt("Inserisci un numero");
+var numeroMaggiore;
+var pari;
+
+if (numeroA > numeroB){
+  numeroMaggiore = numeroA;
+}
+else if (numeroB > numeroA){
+  numeroMaggiore = numeroB;
+}
+else (numeroA == numeroB) {
+  numeroMaggiore = pari;
+}
+
+
+
+
+
+
+
+
+
+
+
+document .getElementById('testo') .innerHTML="Il tuo numero maggiore: " + numeroMaggiore;
