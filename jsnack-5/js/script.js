69f19b89f34a17658a4c5a7c4dc3ed5fb7932b24
Alfonso <alfof87@gmail.com>
2020-06-20 15:36:24 +0200

    jsnack-5

--- jsnack-5/js/script.js
+++ jsnack-5/js/script.js
@@ -1,15 +1,29 @@
+//Nella pagina ci sono due input(num1, num2) e una select con options
+//(somma,sottrai,moltiplica,dividi).
+//Al click su un bottone, il programma inserisce in un p#result il risultato
+//dell'operazione scelta.
+var btn = document.getElementById("btn");
 
-var somma = num1 + num2;
-var btn;
-
-document.getElementById('btn').addEventListener("click", function(){
+document.getElementById("btn").addEventListener("click", function(){
   var num1 = document.getElementById('num1').value;
   var num2 = document.getElementById('num2').value;
+  var op = document.getElementById("operazioni").value;
+  var result = document.getElementById('result');
+//  var somma = document.getElementById('somma') = num1 + num2;
+//  var sottrazione = document.getElementById('sottrazione') = num1 - num2;
+//  var moltiplicazione = document.getElementById('moltiplicazione') = num1 * num2;
+//  var divisione = document.getElementById('divisione') = num1 / num2;
+//  var result = document.getElementById('result') =
 
-})
+  if (op == somma){
+    result.innerHTML = num1 + num2;
+  }else if (op == sottrazione) {
+    result.innerHTML = num1 - num2;
+  }else if (op == moltiplicazione) {
+    result.innerHTML = num1 * num2;
+  }else {
+    result.innerHTML = num1 / num2;
+  }
 
 
-if (num1 + num2){
-  num1 + num2 = somma;
-  console.log(num1 + num2 = somma);
-}
+});
