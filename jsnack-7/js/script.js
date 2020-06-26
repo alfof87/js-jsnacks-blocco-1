4f4b4b71ed38808fac929b7c0a2e4662b5dc9b7c
Alfonso <alfof87@gmail.com>
2020-06-26 17:39:35 +0200

    jsnack-7

---
+++ jsnack-7/js/script.js
@@ -0,0 +1,16 @@
+//Stampa le potenze di 2 fino a 1000
+
+
+// var pot;
+//
+// for(i = 0; i <= 1000; i++){
+//
+//   var pot = (i**2);
+//
+// console.log(pot);
+// }
+
+
+for (i = 1; i < 1000; i = i*2){
+  console.log(i);
+}
