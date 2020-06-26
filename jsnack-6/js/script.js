5c6d76cd5d585c67934463ef7aa94bc949b89bd0
Alfonso <alfof87@gmail.com>
2020-06-26 16:53:36 +0200

    jsnack-6 problema var

---
+++ jsnack-6/js/script.js
@@ -0,0 +1,15 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero
+// indicato dall’utente.
+
+
+
+
+
+
+
+var n = prompt("inserisci un numero");
+
+for(i = 0; i < n; i++){
+ n = (i*i*i);
+console.log(n);
+}
